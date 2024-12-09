bf9d4d565d1786d845ba61612991803a6f61d83d
joe fathalla <joefathalla12@gmail.com>
2024-12-09 17:52:59 +0200

    initial app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// App.tsx
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold mb-4 text-center">Ticket List</h1>
+    </div>
+  );
+};
+
+export default App;
